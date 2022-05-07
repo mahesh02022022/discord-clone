2e9b97781095765b51f1dc8c5de59d1399ff658e
mahesh02022022 <mahesh02022022+github@gmail.com>
2022-05-07 22:48:31 +0530

    Initial commit

---
+++ precache-manifest.27950da7328833a51141a0e1cb0c0bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564b1df40f96f82cea913b62f6e60272",
+    "url": "/discord-clone/index.html"
+  },
+  {
+    "revision": "289aa5b4ff1cd5ac95f3",
+    "url": "/discord-clone/static/css/main.7d69c3e1.chunk.css"
+  },
+  {
+    "revision": "c527d086f986144c0746",
+    "url": "/discord-clone/static/js/2.b02477d7.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/discord-clone/static/js/2.b02477d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289aa5b4ff1cd5ac95f3",
+    "url": "/discord-clone/static/js/main.0bd84604.chunk.js"
+  },
+  {
+    "revision": "340f52defe361cb96065",
+    "url": "/discord-clone/static/js/runtime-main.0b2b6ed2.js"
+  }
+]);
